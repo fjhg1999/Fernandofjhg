c521abe03887806a061086e13fc35047e22fe01d
Fernando <fjhg1999@gmail.com>
2024-02-02 13:57:57 -0600

    CREACION DE LAS PESTAÑAS DEL CONVERSOR

--- app/src/main/java/com/example/calculadoraprogramacion2/MainActivity.java
+++ app/src/main/java/com/example/calculadoraprogramacion2/MainActivity.java
@@ -6,15 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TabHost tbh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tbh= findViewById(R.id.tbhConversor);
 
-
+        tbh.setup();
+        tbh.addTab(tbh.newTabSpec("LON").setContent(R.id.tab1Longitud).setIndicator("LONGITUD", null));
+        tbh.addTab(tbh.newTabSpec("ALM").setContent(R.id.tab1Longitud).setIndicator("ALMACENAMIENTO", null));
+        tbh.addTab(tbh.newTabSpec("MON").setContent(R.id.tab1Longitud).setIndicator("MONEDAS", null));
     }
 }
