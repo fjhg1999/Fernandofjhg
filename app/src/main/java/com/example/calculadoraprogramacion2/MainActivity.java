f7da33986c39802b3e149d94c96ff48d57743e3c
Fernando <fjhg1999@gmail.com>
2024-02-22 20:39:23 -0600

    sensor acelerometro

--- app/src/main/java/com/example/calculadoraprogramacion2/MainActivity.java
+++ app/src/main/java/com/example/calculadoraprogramacion2/MainActivity.java
@@ -1,74 +1,53 @@
 package com.example.calculadoraprogramacion2;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RadioGroup;
 import android.widget.TextView;
-
-public class MainActivity extends AppCompatActivity {
+public final class MainActivity extends AppCompatActivity {
+    SensorManager SensorManager;
+    Sensor sensor;
+    private SensorEventListener _sensorEvenListener;
     TextView tempVal;
-    Button btn;
-    RadioGroup opt;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn = findViewById(R.id.btnCalcular);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View view) {
-                tempVal = findViewById(R.id.txtnum1);
-                double num1 = Double.parseDouble(tempVal.getText().toString());
-
-                tempVal = findViewById(R.id.txtnum2);
-                double num2 = Double.parseDouble(tempVal.getText().toString());
-
-                double respuesta = 0;
-                opt= findViewById(R.id.optOpciones);
-                if (opt.getCheckedRadioButtonId()==R.id.optSuma){
-                    respuesta=num1+num2;
-                }
-                if (opt.getCheckedRadioButtonId()==R.id.optResta){
-                    respuesta=num1-num2;
-                }
-                if (opt.getCheckedRadioButtonId()==R.id.optMultiplicacion){
-                    respuesta=num1*num2;
-                }
-                if (opt.getCheckedRadioButtonId()==R.id.optDivision){
-                    respuesta=num1/num2;
-                }
-
-                if (opt.getCheckedRadioButtonId()==R.id.optFactorial){
-                    double factorial =1;
-                    for (double i=1 ;i <=num1; i++){
-                        factorial*=i;
-                    }
-                    respuesta=factorial;
-                }
-                if (opt.getCheckedRadioButtonId()==R.id.optExponencial){
-                    Math math = null;
-                    respuesta= Math.pow(num1,num2);
-                }
-                if (opt.getCheckedRadioButtonId()==R.id.optPorcentaje){
-                    respuesta=(num1*num2)/100;
-                }
-                if (opt.getCheckedRadioButtonId()==R.id.optRaiz){
-
-                    Math math = null;
-                    respuesta= Math.pow(num1,1/num2);
-
-
-               }
-
-                tempVal= findViewById(R.id.lblrespuesta);
-                tempVal.setText("respuesta:"+respuesta);
+        tempVal = findViewById(R.id.lblSensorAcelerometro);
+        activarSensorAcelerometro();
+    }
+    private void activarSensorAcelerometro() {
+        SensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = SensorManager.getDefaultSensor(sensor.TYPE_ACCELEROMETER);
+        if (sensor == null) {
+            tempVal.setText("tu dispositivo no cuenta con el sensor acelerometro");
+            finish();
+        }
+        _sensorEvenListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                tempVal.setText("acelerometro:x = "+ event.values[0]+", y = "+event.values[1]+", z = "+ event.values[2]);
             }
-        });
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {};
+        };
+    }
+    @Override
+    public void onResume(){
+        iniciar();
+        super.onResume();
+    }
+    @Override
+    public void onPause(){
+        detener();
+        super.onPause();
+    }
+    private void iniciar(){
+        SensorManager.registerListener(_sensorEvenListener, sensor, 2000*1000);
+    }
+    private void detener(){
+        SensorManager.unregisterListener(_sensorEvenListener);
     }
 }
