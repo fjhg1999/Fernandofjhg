d8963e020a40b9ff3038e8fd6f7285cf4d592b9e
Fernando <fjhg1999@gmail.com>
2024-02-23 00:30:00 -0600

    rama de sensor Gps

--- app/src/main/java/com/example/calculadoraprogramacion2/MainActivity.java
+++ app/src/main/java/com/example/calculadoraprogramacion2/MainActivity.java
@@ -1,64 +1,59 @@
 package com.example.calculadoraprogramacion2;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
 public final class MainActivity extends AppCompatActivity {
-    SensorManager SensorManager;
-    Sensor sensor;
-    private SensorEventListener _sensorEvenListener;
     TextView tempVal;
-    @Override
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tempVal = findViewById(R.id.lblSensorDeAproximidad);
-        activarSensorDeLuz();
-    }
-    private void activarSensorDeLuz() {
-        SensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = SensorManager.getDefaultSensor(sensor.TYPE_PROXIMITY);
-        if (sensor == null) {
-            tempVal.setText("tu dispositivo no cuenta con el sensor acelerometro");
-            finish();
-        }
-        _sensorEvenListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                double valor= event.values[0];
-                tempVal.setText("Proximidad"+ valor);
-                if (valor<=20){
-                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-                } else if (valor<=50){
-                    getWindow().getDecorView().setBackgroundColor(Color.RED);
-                } else if (valor>50) {
-                    getWindow().getDecorView().setBackgroundColor(Color.CYAN);
+        tempVal = findViewById(R.id.lblSensorGps);
 
-                }
+    }
+    private void obtenerPosicion(){
+        try {
+
+
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION}, 1);
+                tempVal.setText("solicitando permisos de localizacion...");
             }
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {};
-        };
-    }
-    @Override
-    public void onResume(){
-        iniciar();
-        super.onResume();
-    }
-    @Override
-    public void onPause(){
-        detener();
-        super.onPause();
-    }
-    private void iniciar(){
-        SensorManager.registerListener(_sensorEvenListener, sensor, 2000*1000);
-    }
-    private void detener(){
-        SensorManager.unregisterListener(_sensorEvenListener);
+            Location location = new Location(LocationManager.GPS_PROVIDER);
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            mostrarPosicion(location);
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    mostrarPosicion(location);
+                }
+            };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+        }catch (Exception e){
+            tempVal.setText(e.getMessage());
+        }
+    }private void mostrarPosicion(Location location){
+        tempVal.setText("posicion; latitud:"+location.getLatitude() +"; longitud: "+location.getLongitude()+"; altitud; "+location.getAltitude());
     }
 }
