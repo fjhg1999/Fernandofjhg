a9a9413d85fc32470383c7dbf38c17ee8c8510e0
Fernando <fjhg1999@gmail.com>
2024-03-01 12:03:31 -0600

    Agenda amigos

--- app/src/main/java/com/example/calculadoraprogramacion2/MainActivity.java
+++ app/src/main/java/com/example/calculadoraprogramacion2/MainActivity.java
@@ -9,5 +9,11 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+
+    }
 }
